50602762cb1c25472d734e67c97669e3b8a7fe27
Anointing Maxwell <anointingmax@gmail.com>
2023-02-02 08:46:51 +0100

    Finished footer component

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./footer.module.css";
+import { ReactComponent as ProfileIcon } from "../../assets/img/profile.svg";
+import { ReactComponent as ShoppingBagIcon } from "../../assets/img/shopping-bag.svg";
 
 const Footer = () => {
 	return (
 		<div className="mt-auto">
+			<div
+				className={`${styles.bt} d-flex justify-content-center align-items-center mt-5`}
+			>
+				<ul className={styles.navLinks}>
+					<li className={styles.navlink}>
+						<Link to="#" className={styles.link}>
+							<ProfileIcon />
+							<p>My Account</p>
+						</Link>
+					</li>
+					<li className={styles.navlink}>
+						<Link to="/cart" className={styles.link}>
+							<ShoppingBagIcon />
+							<p>Shopping Bag</p>
+						</Link>
+					</li>
+				</ul>
+			</div>
 			<div
 				className={`${styles.bt} d-flex justify-content-between align-items-center`}
 			>
