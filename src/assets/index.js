118f68580eab645a360b4ad2dd0334263d199d62
Anointing Maxwell <anointingmax@gmail.com>
2023-02-04 09:40:13 +0100

    Fixed assets directory

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,23 @@
-import Bg from "./img/bg.png"
-import { ReactComponent as CartSVG } from "./img/svg/shopping-bag.svg"
-import { ReactComponent as Checked } from "./img/svg/checked.svg"
-import Product from "./img/product.png"
-import Product2 from "./img/product2.png"
+import Bg from "./img/bg.png";
+import { ReactComponent as CartSVG } from "./img/svg/shopping-bag.svg";
+import { ReactComponent as Checked } from "./img/svg/checked.svg";
+import { ReactComponent as FacebookIcon } from "./img/svg/facebook.svg";
+import { ReactComponent as InstagramIcon } from "./img/svg/instagram.svg";
+import { ReactComponent as ProfileIcon } from "./img/svg/profile.svg";
+import { ReactComponent as WhatsAppIcon } from "./img/svg/whatsapp.svg";
+import { ReactComponent as VisaIcon } from "./img/svg/visa.svg";
+import Product from "./img/product.png";
+import Product2 from "./img/product2.png";
 
-export { Bg, Product2, Product, CartSVG, Checked }
+export {
+	Bg,
+	Product2,
+	Product,
+	CartSVG,
+	Checked,
+	FacebookIcon,
+	InstagramIcon,
+	ProfileIcon,
+	WhatsAppIcon,
+	VisaIcon,
+};
